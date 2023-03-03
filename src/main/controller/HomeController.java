8175b4d284c0e6dd74e2359cdda655c79ce48adf
ming <ming@example.com>
2023-03-03 17:52:28 +0800

    index 修改

--- src/main/controller/HomeController.java
+++ src/main/controller/HomeController.java
@@ -1,15 +1,23 @@
 package main.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import main.model.Product;
+import main.service.ProductService;
 
 @Controller
 public class HomeController {
     @Autowired
+    private ProductService productService;
   @RequestMapping("/")
-	public String getHome() {
+	public String getHome(Model model) {
+    	List<Product>products = productService.getAll();
+    	model.addAttribute("products", products);
 
 		
 	        return "index";
